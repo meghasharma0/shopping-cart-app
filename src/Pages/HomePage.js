cb1c53b580630e5e2d00810125c9879278113b0a
Megha Sharma <meghaalg7@gmail.com>
2024-02-06 09:15:33 +0530

    updated homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -9,6 +9,7 @@ const HomePage = () => {
   const [dataDisplayed, setDataDisplayed] = useState(allData);
   const [cartNo, setCartNo] = useState(0);
   const [search, setSearch] = useState("");
+  const [cartMsg, setCartMsg] = useState("");
   const navigate = useNavigate();
 
   // Context
@@ -38,21 +39,30 @@ const HomePage = () => {
 
   // Handle Clothing Click
   const handleClothingClick = () => {
-    const clothingData = allData.filter((obj) => obj.category.includes("clothing"));
+    const clothingData = allData.filter((obj) => obj.category.includes("clothing") && obj.title !== "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops");
     setDataDisplayed(clothingData);
   };
 
   // Handle Accessories Click
-  const handleAccessoriesClick = () => {
-    const accessoriesData = allData.filter((obj) => !obj.category.includes("clothing"));
+  const handleJeweleryClick = () => {
+    const accessoriesData = allData.filter((obj) => obj.category.includes("jewelery"));
     setDataDisplayed(accessoriesData);
+  }
+
+  // Handle Electronic Click
+  const handleElectronicsClick = () => {
+    const electronicData = allData.filter((obj) => obj.category.includes("electronic"));
+    setDataDisplayed(electronicData);
   }
 
   // Handle Search
   const handleSearch = () => {
-    const filteredData = allData.filter((obj) =>
-      obj.title.toLowerCase().includes(search.toLowerCase())
-    );
+    const filteredData = allData.filter((obj) =>{
+      return obj.title.toLowerCase().includes(search.toLowerCase()) || obj.description.toLowerCase().includes(search.toLowerCase())
+    });
+    if (filteredData.length === 0){
+      setCartMsg("No such item present!");
+    }
     setDataDisplayed(filteredData);
   }
 
@@ -67,13 +77,16 @@ const HomePage = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 pt-lg-3">
               <li className="nav-item">
-                <p className="nav-link" onClick={handleClick}>Home</p>
+                <p className="nav-link" onClick={handleClick} style={{ cursor: "pointer" }}>Home</p>
               </li>
               <li className="nav-item">
-                <p className="nav-link" onClick={handleClothingClick}>Clothing</p>
+                <p className="nav-link" onClick={handleClothingClick} style={{ cursor: "pointer" }}>Clothing</p>
               </li>
               <li className="nav-item">
-                <p className="nav-link" onClick={handleAccessoriesClick}>Accessories</p>
+                <p className="nav-link" onClick={handleJeweleryClick} style={{ cursor: "pointer" }}>Jewelery</p>
+              </li>
+              <li className="nav-item">
+                <p className="nav-link" onClick={handleElectronicsClick} style={{ cursor: "pointer" }}>Electronics</p>
               </li>
             </ul>
             <a className="nav-link me-3" href="#">
@@ -82,7 +95,7 @@ const HomePage = () => {
             </a>
             <form className="d-flex" role="search">
               <input className="form-control me-2" type="search" placeholder="Search" onChange={(e) => setSearch(e.target.value)} value={search} aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit" onClick={handleSearch}>Search</button>
+              <button className="btn btn-outline-success" onClick={handleSearch}>Search</button>
             </form>
           </div>
         </div>
@@ -95,6 +108,7 @@ const HomePage = () => {
             <div className="text-center">
               <div className="row mt-4">
 
+              <h4>{cartMsg}</h4>
               {/* Card items */}
                   {
                     dataDisplayed.map((obj) => {
@@ -106,13 +120,13 @@ const HomePage = () => {
                               <img src={obj.image} className="w-100" height="300px"/>
                             </div>
                             <div className="card-body">
-                              <a href="" className="text-reset" style={{textDecoration: "none"}}> <p className="card-title mb-2"><b>{obj.title}</b></p> </a>
-                              <a href="" className="text-reset "  style={{textDecoration: "none"}}> <p>{obj.description}</p> </a>
+                              <a href="" className="text-reset" style={{textDecoration: "none"}}> <p className="card-title mb-2"><b>{obj.title.slice(0, 20)}...</b></p> </a>
+                              <a href="" className="text-reset "  style={{textDecoration: "none"}}> <p>{obj.description.slice(0, 50)}...</p> </a>
                               <h6 className="mb-3 price">${obj.price}</h6>
                               <button className="btn btn-primary" onClick={() => {
                                 cntxt.setItems([ 
                                   ...cntxt.items, 
-                                  { id: obj.id, title: obj.title, image: obj.image, description: obj.description, price: obj.price  }
+                                  { id: obj.id, title: obj.title, image: obj.image, description: obj.description, price: obj.price }
                                  ]); 
                                  cntxt.setTotalAmt(cntxt.totalAmt + obj.price);
                                  setCartNo(cntxt.items.length + 1);
